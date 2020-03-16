13b0d10cf670e92e32ef947100a1e75b6731e9fa
thomaspernet <t.pernetcoudrier@gmail.com>
2020-03-16 09:29:09 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from AWS_connector import aws_instantiate
-
-con = aws_instantiate(credential = ['AKIAI6S4IIWX62NTXIYQ',
- 'CTdVw+mV+ajPEYUTJzvga52jaKZABnSqKGOEvhUZ'], region = 'us-east-1')
-
-con.load_credential()
-client= con.client_boto()
-
-client['s3']
