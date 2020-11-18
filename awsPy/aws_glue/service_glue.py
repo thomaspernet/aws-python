86e697e6d1d3b0e1b816dbfc89a0ed543ed9360e
thomaspernet <t.pernetcoudrier@gmail.com>
2020-11-18 07:19:44 +0100

    Add crawler to update data catalog

--- awsPy/aws_glue/service_glue.py
+++ awsPy/aws_glue/service_glue.py
@@ -68,3 +68,134 @@ class connect_glue():
         )
 
         return update
+
+    def create_table_glue(self, target_S3URI, name_crawler, Role, DatabaseName,
+    TablePrefix, from_athena=True, update_schema=None):
+        """
+        Create a table in Glue data catalog
+
+        Args:
+
+        - target_S3URI: string. S3 Location of the files to parse
+        - name_crawler: string. Define a crawler name
+        - Role: string. ARN role nam, ie. arn:aws:iam::468786073381:role/AWSGlueServiceRole-XXX
+        - DatabaseName: string. Database name to add the new table
+        - TablePrefix: string. Table prefix. Full name will be TablePrefix + folder name
+        - from_athena;: Boolean. If True, then change the schema as follow:
+            - Serde serialization lib: org.apache.hadoop.hive.serde2.OpenCSVSerde
+            - Serde parameters
+                - escapeChar: \\
+                - quoteChar: "
+                - separatorChar:,
+                - serialization.format: 1
+        - Schema: if not None then List. Update the schema in the catalog. Should be as follow:
+        "schema":[
+                   {
+                      "Name":"",
+                      "Type":"",
+                      "Comment":""
+                   }
+                ]
+
+        return Crawler jobs
+
+        """
+
+        # Get connection
+        #client_glue = client['glue']
+
+        table_name = '{}{}'.format(TablePrefix, os.path.basename(target_S3URI).lower())
+
+        ## Remove table if exist
+        try:
+            response = self.client['glue'].delete_table(
+            DatabaseName=DatabaseName,
+            Name=table_name
+        )
+        except Exception as e:
+            print(e)
+            pass
+
+        # Remove if exist
+        try:
+            self.client['glue'].delete_crawler(
+            Name=name_crawler
+        )
+        except Exception as e:
+            print(e)
+            pass
+
+        self.client['glue'].create_crawler(
+            Name=name_crawler,
+            Role=Role,
+            DatabaseName=DatabaseName,
+            #Description='Parse the symbols filtered by a given timestamp',
+            Targets={
+                'S3Targets': [
+                    {
+                        'Path': target_S3URI,
+                        'Exclusions': [
+                            '*.csv.metadata',
+                        ]
+                    },
+                ],
+            },
+            TablePrefix=TablePrefix,
+            SchemaChangePolicy={
+                'UpdateBehavior': 'LOG',
+                'DeleteBehavior': 'LOG'
+            },
+        )
+
+        # Wait until job is done
+        self.client['glue'].start_crawler(Name=name_crawler)
+        status = None
+        while status == 'RUNNING' or status == None:
+            response_crawler = self.client['glue'].get_crawler(Name=name_crawler)
+            status = response_crawler['Crawler']['State']
+            if (status == 'RUNNING' or status == None):
+                time.sleep(10)
+
+        # Update Schema
+        if from_athena:
+            # update schema serde
+
+            response = self.client['glue'].get_table(
+                DatabaseName=DatabaseName,
+                Name=table_name
+            )['Table']
+
+            serde = {
+                "SerializationLibrary": "org.apache.hadoop.hive.serde2.OpenCSVSerde",
+                "Parameters": {
+                    "escapeChar": "\\",
+                    "quoteChar": '"',
+                    "separatorChar": ",",
+                    "serialization.format": "1",
+                },
+            }
+
+            response['StorageDescriptor']['SerdeInfo'] = serde
+            key_to_remove = ['DatabaseName', 'CreateTime', 'UpdateTime', 'CreatedBy',
+                             'IsRegisteredWithLakeFormation', 'CatalogId']
+            for key in key_to_remove:
+                response.pop(key, None)
+
+            self.client['glue'].update_table(
+                DatabaseName=DatabaseName,
+                TableInput=response
+            )
+
+        # Update schema
+
+        if update_schema != None:
+
+            self.update_schema_table(
+                database=DatabaseName, table=table_name, schema=update_schema)
+
+        response = self.client['glue'].get_table(
+                DatabaseName=DatabaseName,
+                Name=table_name
+            )['Table']
+
+        return response
